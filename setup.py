e38a2a0cd8b0e86cd6dd95e2db96560c72e7579a
David Francos Cuartero <opensource@davidfrancos.net>
2015-01-07 20:23:58 +0100

    Version normalization

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='airoscriptng',
-    version='0.1.0',
+    version='0.0.1',
     description='Airoscript-ng python complete implementation',
     long_description=readme + '\n\n' + history,
     author='David Francos Cuartero',
