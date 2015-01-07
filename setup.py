d477ff496a2206f35828fe638c47c9ebae0947ba
root <root@home>
2015-01-07 19:15:26 +0100

    Trying to move tests to _tests to see if travis gets it...

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
-    test_suite='tests',
+    test_suite='_tests',
     tests_require=test_requirements
 )
