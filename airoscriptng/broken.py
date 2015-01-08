4de9bf68bddf2c738c8ab7766404f2fbb96db0a5
David Francos Cuartero <opensource@davidfrancos.net>
2015-01-08 01:24:15 +0100

    Rearranged wep attacks, added p0841

--- airoscriptng/broken.py
+++ airoscriptng/broken.py
@@ -7,10 +7,14 @@ PRIVACY = {
     'WPA': [50, ['get_wpa_handshake', 'pyrit']],
     'WPA2': [10, ['get_wpa_handshake',  'pyrit']],
     'WPA2WPA': [10, ['get_wpa_handshake', 'pyrit']],
-    'WEP': [900, ['wep_dissasication', 'wep_caffe_latte', 'wep_fragmentation', 'wep_chopchop' ]]
+    'WEP': [900, ['wep_dissasication', 'wep_caffe_latte', 'wep_p0841', 'wep_chopchop', 'wep_fragmentation']]
 }
 
 def get_hackability_name(point):
+    """
+        Return a more human-understandable name for a
+        hackability statistic
+    """
     hackabilities = {
         20: 'VERY LOW',
         50: 'LOW',
